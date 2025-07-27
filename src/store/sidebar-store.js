91fba89bfd867a54e842aafb972df7367660793d
ASHIN T V <146656061+ashintv@users.noreply.github.com>
2025-07-27 21:58:01 +0530

    converted sidebar store to persist

--- src/store/sidebar-store.js
+++ src/store/sidebar-store.js
@@ -1,10 +1,19 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 // const allowedStates = ["Home", "Task", "Inbox", "Meetings", "Calendar"];
 
-export const useSidebarStore = create((set) => ({
-    current: "Home",
-    setCurrent: (newState) => {
-        set({ current: newState });
-    },
-}));
+export const useSidebarStore = create(
+    persist(
+        (set) => ({
+            current: "Home",
+            setCurrent: (newState) => {
+                set({ current: newState });
+            },
+        }),
+        {
+            name: 'sidebar-storage', // 🔑 unique storage key
+            storage: createJSONStorage(() => sessionStorage), // ✅ use sessionStorage
+        }
+    )
+);
